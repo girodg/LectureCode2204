a0e5bf6fe37f78421668cdc512bdefe629cb8be2
Garrett Girod <girodg@hotmail.com>
2022-04-04 10:15:37 -0400

    day01

---
+++ Day01/Day01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Day01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintMessage();
+        }
+
+        static void PrintMessage()
+        {
+            Console.WriteLine("Welcome to Gotham.");
+        }//ctrl+k,d to fix formatting
+    }
+
+}
