de686b73016cec157935839e9a40751e85b29aaa
Garrett Girod <girodg@hotmail.com>
2022-04-04 10:38:14 -0400

    methods

--- Day01/Day01/Program.cs
+++ Day01/Day01/Program.cs
@@ -7,12 +7,29 @@ namespace Day01
         static void Main(string[] args)
         {
             PrintMessage();
+            string msg = GetMessage();
+            int[] nums;// = new int[5];
+            int n1 = 15;
+            int n2 = 2;
+            int sum = Add(n1, n2);
+        }
+
+        static int Add(int num1, int num2)//Pass By Value (think Copy)
+        {
+            num1 += 10;
+            return num1 + num2;
         }
 
         static void PrintMessage()
         {
             Console.WriteLine("Welcome to Gotham.");
         }//ctrl+k,d to fix formatting
+
+        static string GetMessage()
+        {
+            Console.Write("What is your secret identity? ");
+            return Console.ReadLine();
+        }
     }
 
 }
