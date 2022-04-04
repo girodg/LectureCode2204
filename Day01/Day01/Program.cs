d0f6d225ce371b8b4412328cf3084e72722a38e1
Garrett Girod <girodg@hotmail.com>
2022-04-04 10:53:18 -0400

    pass by val/ref

--- Day01/Day01/Program.cs
+++ Day01/Day01/Program.cs
@@ -6,18 +6,34 @@ namespace Day01
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(DateTime.Now);
             PrintMessage();
             string msg = GetMessage();
             int[] nums;// = new int[5];
             int n1 = 15;
             int n2 = 2;
             int sum = Add(n1, n2);
+            PrintMessage(msg);
+
+            Factor(ref n1, 3);//in the method, n1 has a new name (number)
+            Factor(ref n2, 2);//in the method, n2 has a new name (number)
+            Console.WriteLine(n1);
+        }
+
+        static void Factor(ref int number, int factor)
+        {
+            number *= factor; 
         }
 
         static int Add(int num1, int num2)//Pass By Value (think Copy)
         {
             num1 += 10;
             return num1 + num2;
+        }
+
+        static void PrintMessage(string messageToPrint)
+        {
+            Console.WriteLine(messageToPrint);
         }
 
         static void PrintMessage()
