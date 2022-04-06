8d755e765a4cfe6f693f06e9c3e06f17c16ed46c
Garrett Girod <girodg@hotmail.com>
2022-04-06 10:57:58 -0400

    PrintGrades

--- Day02/Day02/Program.cs
+++ Day02/Day02/Program.cs
@@ -7,7 +7,12 @@ namespace Day02
     {
         static void Main(string[] args)
         {
+            #region Arrays
             ArrayChallenge();
+
+            #endregion
+
+            #region Lists
 
             List<bool> flags = new List<bool>(10);// { true, true, false };
 
@@ -29,11 +34,18 @@ namespace Day02
             flags.Add(true);
             PrintInfo(flags);//count: 12 Capacity: 20
             //flags.TrimExcess();//reclaim the unused memory
-
             for (int i = 0; i < flags.Count; i++)
             {
                 Console.WriteLine(flags[i]);
             }
+            #endregion
+
+            #region Challenges
+            Console.WriteLine("Press any key for list challenge...");
+            Console.ReadKey();
+            ListChallenge();
+            #endregion
+
         }
 
         private static void PrintInfo(List<bool> flags)
@@ -61,6 +73,33 @@ namespace Day02
             for (int i = 0; i < 10; i++)
             {
                 grades.Add(rando.NextDouble()*100);
+            }
+            PrintGrades(grades);
+        }
+
+        static void PrintGrades(List<double> grades)
+        {
+            Console.Clear();
+            string header = "---------------GRADES-------------";
+            int x = Console.WindowWidth / 2;
+            int y = Console.WindowHeight / 2 - 5;
+            Console.SetCursorPosition(x - header.Length/2, y);
+            Console.WriteLine(header);
+            foreach (var grade in grades)
+            {
+                //Console.SetCursorPosition(x - header.Length / 2, Console.CursorTop);
+                Console.CursorLeft = x - 3;
+
+                if (grade < 59.5) Console.BackgroundColor = ConsoleColor.Red;
+                else if(grade < 69.5) Console.ForegroundColor = ConsoleColor.DarkYellow;
+                else if (grade < 79.5) Console.ForegroundColor = ConsoleColor.Yellow;
+                else if (grade < 89.5) Console.ForegroundColor = ConsoleColor.Blue;
+                else  Console.ForegroundColor = ConsoleColor.Green;
+
+                // ,6 means right-aligned 6 spaces
+                // :N2 means number with 2 decimal places
+                Console.WriteLine($"{grade,7:N3}");
+                Console.ResetColor();
             }
         }
     }
