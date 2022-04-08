f0093c348b50ab4cc89e034ce5ec25c9ae01a79c
Garrett Girod <girodg@hotmail.com>
2022-04-08 10:09:34 -0400

    printing

--- Day03/Day03/Program.cs
+++ Day03/Day03/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Threading;
 
 namespace Day03
 {
@@ -38,11 +40,17 @@ namespace Day03
             menu["Filet"] = 19.99F;
             menu["Filet"] = 29.99F;//will simply overwrite the value
 
+            //Thread.CurrentThread.CurrentCulture = new CultureInfo("de-DE");
+            //Thread.CurrentThread.CurrentUICulture = new CultureInfo("de-DE");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
             foreach (var menuItem in menu)
             {
                 float price = menuItem.Value;
                 Console.WriteLine($"{price,10:C2} {menuItem.Key}");
             }
+
+            PressAnyKey("Press any key to continue...");
+            Challenges();
         }
 
         static void Challenges()
@@ -66,14 +74,32 @@ namespace Day03
             pg2["Joshua"] = randy.NextDouble() * 100;
             pg2["Arianna"] = randy.NextDouble() * 100;
             pg2["Armando"] = randy.NextDouble() * 100;
+            pg2["Maurico"] = randy.NextDouble() * 100;
+            pg2["Meggy"] = randy.NextDouble() * 100;
+            pg2["Morgen"] = randy.NextDouble() * 100;
+            pg2["Oscar"] = randy.NextDouble() * 100;
 
-            foreach (KeyValuePair<string,double> student in pg2)
+            PrintGrades(pg2);
+        }
+
+        static void PrintGrades(Dictionary<string, double> course)
+        {
+            Console.Clear();
+            Console.WriteLine("-----------GRADES------------");
+            foreach (var student in course)
             {
-                string name = student.Key;
                 double grade = student.Value;
+                Console.Write(student.Key);
+                Console.CursorLeft = 15;
 
+                Console.ForegroundColor = (grade < 59.5) ? ConsoleColor.Red :
+                                          (grade < 69.5) ? ConsoleColor.DarkYellow :
+                                          (grade < 79.5) ? ConsoleColor.Yellow:
+                                          (grade < 89.5) ? ConsoleColor.Blue :
+                                                           ConsoleColor.Green;
+                Console.WriteLine($"{grade,7:N2}");
+                Console.ResetColor();
             }
-
         }
 
         static void PressAnyKey(string prompt)
