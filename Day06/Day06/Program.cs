7e4d26b77097d5074b18d88f1a3fd0b0265e1141
Garrett Girod <girodg@hotmail.com>
2022-04-13 10:15:41 -0400

    swap

--- Day06/Day06/Program.cs
+++ Day06/Day06/Program.cs
@@ -6,6 +6,22 @@ namespace Day06
     {
         static void Main(string[] args)
         {
+            int[] nums = new int[] { 5, 13, 7, 3, 42 };
+            Print(nums);
+            Swap(nums, 1, 2);
+            Print(nums);
+        }
+
+        private static void Print(int[] nums)
+        {
+            foreach (var item in nums)
+                Console.Write($"{item} ");
+            Console.WriteLine();
+        }
+
+        private static void Swap(int[] nums, int index1, int index2)
+        {
+            (nums[index1], nums[index2]) = (nums[index2], nums[index1]);
         }
     }
 }
