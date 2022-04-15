7bf4bb1a3aaa500a948c62259ac4a8be6909c181
Garrett Girod <girodg@hotmail.com>
2022-04-15 11:49:34 -0400

    Read methods

---
+++ Day07/Day07/Input.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day07
+{
+    public static class Input
+    {
+
+        public static int ReadInteger(string prompt, int min, int max)
+        {
+            do
+            {
+                Console.WriteLine(prompt);
+                if (int.TryParse(Console.ReadLine(), out int number) && number >= min && number <= max)
+                    return number;
+                Console.WriteLine($"Error. The number must be >= {min} and <= {max}");
+            } while (true);
+        }
+
+        public static void ReadString(string prompt, ref string value)
+        {
+            do
+            {
+                Console.WriteLine(prompt);
+                value = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(value))
+                    return;
+                Console.WriteLine("Error! please enter something.");
+            } while (true);
+        }
+
+        public static void ReadChoice(string prompt, string[] options, out int selection)
+        {
+            foreach (var item in options)
+                Console.WriteLine(item);
+            selection = ReadInteger(prompt, 1, options.Length);
+        }
+    }
+}
